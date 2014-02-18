e2ecf1f74dfd18c44314ef4b4e94cc289999049c
Zhiqiang Sui <zhiqiang_sui@brown.edu>
2014-02-18 13:31:48 -0500

    finish matrix operations for links

--- 3jsbot_matrix.js
+++ 3jsbot_matrix.js
@@ -17,3 +17,58 @@ generate_rotation_matrix_Z
 */
 
 
+function matrix_multiply(mt1, mt2)
+{
+	mt = [];
+	for (var i = 0; i < mt1.length; i++)
+	{
+		tmp = [];
+		for (var j = 0; j < mt2[i].length; j++)
+		{
+			sum = 0;
+			for (var k = 0; k < mt2.length; k++) 
+				sum = sum + mt1[i][k] * mt2[k][j];
+			tmp[j] = sum;
+		}
+		mt.push(tmp);
+	}
+	
+	return mt
+}
+
+function matrix_transpose(mt)
+{
+	n = mt.length;
+	m = mt[0].length;
+	mtt = [];
+	
+	for (var i = 0 ; i < m; i++)
+	{
+		mtt[i] = [];
+		for (var j = 0 ; j < n; j++)
+		{
+			mtt[i][j] = mt[j][i];
+		}
+	}
+	return mtt;
+}
+
+function generate_identity(n)
+{
+	mt = [];
+	for (var i = 0 ; i < n ; i++)
+	{
+		mt[i] = []
+		for (var j = 0 ; j < n ; j++)
+		{
+			if ( i == j ) mt[i][j] = 1;
+			else mt[i][j] = 0;
+		}
+	}
+	return mt;
+}
+
+function generate_translation_matrix(t1, t2)
+{
+	return matrix_multiply(t1, t2);
+}
